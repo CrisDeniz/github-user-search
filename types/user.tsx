06b636de0a021513ca12465e1c55503ede223471
CrisDeniz <cdgm.7star@gmail.com>
2022-10-21 22:16:57 -0300

    first commit

---
+++ types/user.tsx
@@ -0,0 +1,11 @@
+export type User = {
+    avatar_url: string;
+    repos_url: string;
+    name: string;
+    bio: string;
+    location: string;
+    public_repos: number;
+    public_gists: number;
+    followers: number;
+    following: number;
+}
